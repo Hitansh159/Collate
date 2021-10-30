934b663d4e93a46c15186215ca259f75a5ed217f
Hitansh Doshi <hkd159@gmail.com>
2021-10-31 02:27:12 +0530

    editor text color update

--- client/src/components/editor/input/input.js
+++ client/src/components/editor/input/input.js
@@ -16,7 +16,7 @@ function InputText({label, placeholder}) {
             <label class="label w-full">
                 <span class="card-title text-primary-content">{label}</span>
             </label>
-            <input type="text" placeholder={placeholder} className="w-9/12 max-w-5xl mx-3 my-2 p-2 rounded-lg shadow-lg min-h-12 bg-base-100 hover:bg-base-200 focus:bg-base-200 focus:outline-none focus:ring-2 focus:ring-primary-focus " onChange={click} disabled={!state.edit} required value={state[label.toLowerCase()]}/>
+            <input type="text" placeholder={placeholder} className="text-black w-9/12 max-w-5xl mx-3 my-2 p-2 rounded-lg shadow-lg min-h-12 bg-base-100 hover:bg-base-200 focus:bg-base-200 focus:outline-none focus:ring-2 focus:ring-primary-focus dark:text-white" onChange={click} disabled={!state.edit} required value={state[label.toLowerCase()]}/>
         </div>
     );
 }
