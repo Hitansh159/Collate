a82961470a58e2ac2bd8a925a4cc1b99e853e22e
Hitansh Doshi <hkd159@gmail.com>
2021-10-31 05:01:15 +0530

    updated proxy according to server

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:5000/api" });
+const API = axios.create({ baseURL: "https://collate-api-hack.herokuapp.com/api" });
 
 // //* Adding Authorization Token in req.headers
 // API.interceptors.request.use((req) => {
