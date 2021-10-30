192176628135a5209f780253c0bd0e3668a3803e
Shreyas30 <jshreyas12@gmail.com>
2021-10-30 17:35:03 +0530

    Successful Connect with Server

--- server/app.js
+++ server/app.js
@@ -1,21 +1,25 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var fs = require('fs');
-var logger = require('morgan');
+var express = require("express");
+var cors = require("cors");
+var cookieParser = require("cookie-parser");
+var path = require("path");
+var fs = require("fs");
+var logger = require("morgan");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/api');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/api");
 
 var app = express();
-var accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' })
-app.use(logger('common', {stream: accessLogStream}));
+app.use(cors());
+var accessLogStream = fs.createWriteStream(path.join(__dirname, "access.log"), {
+  flags: "a",
+});
+app.use(logger("common", { stream: accessLogStream }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/api', usersRouter);
+app.use("/", indexRouter);
+app.use("/api", usersRouter);
 
 module.exports = app;
